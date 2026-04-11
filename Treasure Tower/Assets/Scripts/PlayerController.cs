c803a487bfbab7b615d52bd14d1fd81e24979b16
NorthernFaux <shannon.smith17@dcmail.ca>
2026-04-10 23:28:28 -0400

    quick comment

--- Treasure Tower/Assets/Scripts/PlayerController.cs
+++ Treasure Tower/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,12 @@
 using UnityEngine;
 
+// attach to the character objects
+// allows the characters to move, interacting with the game grid
 public class PlayerController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    void Start() { }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void Update() { }
 }
