10b0999a2c16c9602986e4810c6402036dbfabb3
NorthernFaux <shannon.smith17@dcmail.ca>
2026-04-12 01:17:33 -0400

    tile enhancements, tile attack in room scaffolding

--- Treasure Tower/Assets/Scripts/BossRoom.cs
+++ Treasure Tower/Assets/Scripts/BossRoom.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -14,9 +15,21 @@ public class BossRoom : MonoBehaviour
         private bool Passable;
         private Status? tileStatus;
 
+        // controls the tile attacks on the grid
+        // lifetime dictates how long the tile attack will last
+        private TileAttack? tileAttack;
+        private int tileAttackLifetime = 0;
+
         // WIP
-        // tile visual highlighting, updated similarly to status in BossRoom
-        private bool isHighlighted;
+        // tile visual highlighting so we don't inflict damage on the player yet
+        private bool highlighted;
+
+        // WIP - may want to store original tile here, to make updating easier
+
+        public int GetLifetime()
+        {
+            return this.tileAttackLifetime;
+        }
 
         public void SetPassable(bool isPassable)
         {
@@ -28,11 +41,17 @@ public class BossRoom : MonoBehaviour
             return this.Passable;
         }
 
-        public void SetHighlighted(bool highlighted)
+        public bool IsHighlighted()
         {
-            this.isHighlighted = highlighted;
+            return this.highlighted;
         }
 
+        public void SetHighlighted(bool highlighted)
+        {
+            this.highlighted = highlighted;
+        }
+
+        // checks if the tile has a perma status (eg: lava, impassable barrier, etc)
         public bool HasStatus()
         {
             if (this.tileStatus is null)
@@ -42,14 +61,62 @@ public class BossRoom : MonoBehaviour
             return true;
         }
 
+        // sets the tile's status
+        // only run on game start
         public void SetStatus(Status? newStatus)
         {
             this.tileStatus = newStatus;
         }
 
+        // TILE ATTACK HANDLING
+        // this sets the tile attack
+        // non nullable
+        public void SetTileAttack(TileAttack newTileAttack, int lifetime, bool highlighted)
+        {
+            if (tileAttackLifetime > 0)
+            {
+                return;
+            }
+
+            this.tileAttack = newTileAttack;
+            this.tileAttackLifetime = lifetime;
+            this.highlighted = highlighted;
+        }
+
+        // checks if there is a tile attack
+        public bool HasTileAttack()
+        {
+            if (this.tileAttack is null)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public TileAttack GetTileAttack()
+        {
+            return this.tileAttack;
+        }
+
+        public void ReduceTileAttackLifetime()
+        {
+            this.tileAttackLifetime -= 1;
+            if (this.tileAttackLifetime <= 0)
+            {
+                RemoveTileAttack();
+            }
+        }
+
+        // removes the tile attack when it's no longer a factor or lifetime expires
+        public void RemoveTileAttack()
+        {
+            this.tileAttack = null;
+            this.tileAttackLifetime = 0;
+            this.highlighted = false;
+        }
+
         // status needs some more specific functions, WIP
-        // func for getting the tile sprite
-        // func for reducing lifetime (if applicable)
+        // func for changing the tile sprite
         //
         // func for BossRoom to redraw tiles
         public Status GetStatus()
@@ -90,6 +157,10 @@ public class BossRoom : MonoBehaviour
 
     [SerializeField]
     private Tile[] ImpassableTiles;
+
+    // to control if the game should look for tile attacks on grid to do calculations
+    // also allows the boss to make one if there isn't an active one yet
+    private bool TileAttackOnGrid = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -133,6 +204,10 @@ public class BossRoom : MonoBehaviour
         {
             // controls if the entire row should be impassable
             bool impassableRow = false;
+            if (NumPlayableXTiles[y] == 0)
+            {
+                impassableRow = true;
+            }
 
             // maps where the impassable X values are
             // left is where they END for that side
@@ -177,6 +252,173 @@ public class BossRoom : MonoBehaviour
                 roomData[x, y].SetHighlighted(false);
 
                 roomTilemap.SetTile(new Vector3Int(x, y, 0), tile);
+            }
+        }
+    }
+
+    // handles all of the aspects of updating the room
+    // does calculations for tile attack changes
+    // changes the room sprites accordingly
+    public void RefreshRoom(TileAttack? newTileAttack)
+    {
+        // collects all playable tile locations
+        // they must both be passable and not affected by a tileattack
+        List<Vector2Int> allowableLocations = new List<Vector2Int>();
+        // and the locations to update
+        List<Vector2Int> updateLocations = new List<Vector2Int>();
+
+        int updatedLifetime = 0;
+
+        bool isNewTileAttack = true;
+        if (newTileAttack is null)
+        {
+            isNewTileAttack = false;
+        }
+
+        // if we're processing tile spread or not
+        bool doesSpread = false;
+
+        // grid doesn't need to be updated
+        if (!this.TileAttackOnGrid && !isNewTileAttack)
+        {
+            return;
+        }
+
+        // start updating the grid by looping through/checking all the grid data
+        for (int y = 0; y < RoomHeight; ++y)
+        {
+            // stop the loop early if it's an impassable row, nothing to change
+            if (NumPlayableXTiles[y] == 0)
+            {
+                continue;
+            }
+
+            for (int x = 0; x < RoomWidth; ++x)
+            {
+                // so we can access the tile's information
+                TileData tileData = roomData[x, y];
+
+                // if it's an impassable tile, there is nothing to update
+                if (!tileData.IsPassable())
+                {
+                    continue;
+                }
+
+                // swap the highlighted tiles to one that has an attack
+                if (tileData.IsHighlighted())
+                {
+                    tileData.SetHighlighted(false);
+                    updateLocations.Add(new Vector2Int(x, y));
+                    continue;
+                }
+
+                // active tile attack updating
+                if (tileData.HasTileAttack())
+                {
+                    updateLocations.Add(new Vector2Int(x, y));
+                    // reduce the lifetime
+                    // then, if the tile attack is over, stop this loop
+                    // yes it's going to set the tileattackongrid multiple times idc
+                    tileData.ReduceTileAttackLifetime();
+                    if (!tileData.HasTileAttack())
+                    {
+                        this.TileAttackOnGrid = false;
+                        continue;
+                    }
+                    // this tile will be free to add a new attack to in a moment
+                    if (tileData.GetTileAttack().GetDoesSpread())
+                    {
+                        doesSpread = true;
+                        updatedLifetime = tileData.GetLifetime();
+                        allowableLocations.Add(new Vector2Int(x, y));
+                    }
+                }
+                else
+                {
+                    // this tile will be free to add an attack to
+                    allowableLocations.Add(new Vector2Int(x, y));
+                }
+            }
+        }
+
+        // process attack spread
+        // or new attacks on the grid
+        if (doesSpread)
+        {
+            List<Vector2Int> newAffectedTiles = new List<Vector2Int>();
+
+            // pass the updatable (spreadable) tiles to the tile attack handler
+            // and the allowable tiles
+
+            // loop through the newly affected tiles
+            // set sprites and their TileAttack variables
+            // lifetimes have already been handled previously
+            //
+            // if there are no tiles (all attacks spread off board or no possible tiles)
+            // then skip this and fix vars
+            if (newAffectedTiles.Count > 0)
+            {
+                foreach (Vector2Int location in newAffectedTiles)
+                {
+                    int x = location.x;
+                    int y = location.y;
+
+                    if (roomData[x, y] is null)
+                    {
+                        continue;
+                    }
+
+                    roomData[x, y].SetTileAttack(newTileAttack, updatedLifetime, false);
+                    // WIP - sprite processing here
+                    // note - make a method for this, it's called 3 times
+                }
+            }
+            else
+            {
+                this.TileAttackOnGrid = false;
+            }
+        }
+        else if (isNewTileAttack)
+        {
+            List<Vector2Int> newAffectedTiles = new List<Vector2Int>();
+            this.TileAttackOnGrid = true;
+
+            // pass the allowable tiles to the tile attack handler
+
+            // loop through the affected tiles
+            // set up the tileattack on them, as long as the tile exists
+            foreach (Vector2Int location in newAffectedTiles)
+            {
+                int x = location.x;
+                int y = location.y;
+
+                if (roomData[x, y] is null)
+                {
+                    continue;
+                }
+                roomData[x, y]
+                    .SetTileAttack(newTileAttack, newTileAttack.GetTileAttackDuration(), true);
+                // WIP - sprite processing here
+                // these would be highlighted tiles!
+            }
+        }
+
+        // process updating the tiles to match their sprites
+        // WIP - need to know how to do this first
+
+        // loop through all updatable tiles
+        if (updateLocations.Count > 0)
+        {
+            foreach (Vector2Int location in updateLocations)
+            {
+                int x = location.x;
+                int y = location.y;
+
+                if (roomData[x, y] is null)
+                {
+                    continue;
+                }
+                // WIP - sprite processing here
             }
         }
     }
--- Treasure Tower/Assets/Scripts/TileAttack.cs
+++ Treasure Tower/Assets/Scripts/TileAttack.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 /// Subclass of Attack used exclusively by bosses.
 /// Handles tile-based attacks that affect areas of the board.
 /// Boss tile attacks have no range limit — they can target anywhere on the playable grid.
-/// When spreading, always expands outward in a square (all 8 neighbors).
 /// </summary>
 public class TileAttack : Attack
 {
@@ -13,7 +12,7 @@ public class TileAttack : Attack
     /// How many turns this tile attack remains active on the board.
     /// </summary>
     [SerializeField]
-    private int tileAttackDuration;
+    private int tileAttackLifetime;
 
     /// <summary>
     /// How many tiles are affected when this attack is first placed.
@@ -25,31 +24,41 @@ public class TileAttack : Attack
     /// Whether this attack spreads to adjacent tiles each turn.
     /// </summary>
     [SerializeField]
-    private bool doesSpread;
+    private bool doesSpread = false;
 
     /// <summary>
     /// The tiles currently affected by this attack.
     /// Stored as Vector2Int (x, y). Z is always 0 on our board.
     /// </summary>
+    // DELETE - handled by BossRoom update
     private List<Vector2Int> currentTiles = new List<Vector2Int>();
 
     /// <summary>
     /// When true, tiles are highlighted as a warning (shown the turn before the attack lands).
     /// Always initialized to true — the first turn is always a prediction.
     /// </summary>
+    // DELETE - handled by individual tiles instead
     private bool showPrediction = true;
 
     /// <summary>
     /// Tracks whether this attack has already spread once this boss fight.
     /// Spread only happens once — after that the tile set stays fixed.
     /// </summary>
+    // DELETE - handled by BossRoom
     private bool hasSpread = false;
+
+    /// <summary>
+    /// the shape of the attack (how it spreads/starts)
+    /// by default, it has no shape and only exists as a singular tile (static)
+    /// acceptable terms: plus, square, circle, row, double row, wave, double wave
+    /// </summary>
+    private string shape = "static";
 
     // --- Getters ---
 
     public int GetTileAttackDuration()
     {
-        return this.tileAttackDuration;
+        return this.tileAttackLifetime;
     }
 
     public int GetInitialTilesAffected()
@@ -62,6 +71,7 @@ public class TileAttack : Attack
         return this.doesSpread;
     }
 
+    // DELETE - all 3 handled elsewhere
     public bool GetShowPrediction()
     {
         return this.showPrediction;
@@ -84,10 +94,164 @@ public class TileAttack : Attack
     /// Returns true if the attack is still active, false if it should be destroyed.
     /// Called by Game before SpreadAttack each turn.
     /// </summary>
+    // DELETE - lifetimes handled by tiles instead
     public bool AvailableTileAttackDuration()
     {
-        this.tileAttackDuration -= 1;
-        return this.tileAttackDuration > 0;
+        this.tileAttackLifetime -= 1;
+        return this.tileAttackLifetime > 0;
+    }
+
+    /// <summary>
+    /// generates the initial location for the attack
+    /// </summary>
+    public List<Vector2Int> PlaceAttack(
+        List<Vector2Int> allowableTiles,
+        int firstRow,
+        int gridLength
+    )
+    {
+        List<Vector2Int> affectedTiles = new List<Vector2Int>();
+        // this later checks if something is in the allowable tile list
+        // because items in allowable are removed over time, it also prevents duplication
+        List<Vector2Int> possibleAffectedTiles = new List<Vector2Int>();
+        int amountToPlace = initialTilesAffected;
+
+        // quick check so nothing is broken
+        if (amountToPlace < allowableTiles.Count)
+        {
+            amountToPlace = System.Convert.ToInt32(allowableTiles.Count / 2);
+        }
+
+        // rows
+        // these only need our starting row, and how wide the grid is
+        if (shape == "row" || shape == "double row")
+        {
+            for (int x = 0; x < gridLength; x++)
+            {
+                Vector2Int location = new Vector2Int(x, firstRow);
+                if (allowableTiles.Contains(location))
+                {
+                    affectedTiles.Add(location);
+                }
+                if (shape == "double row")
+                {
+                    location = new Vector2Int(x, firstRow + 1);
+                    if (allowableTiles.Contains(location))
+                    {
+                        affectedTiles.Add(location);
+                    }
+                }
+            }
+        }
+
+        // WIP - waves
+        // does nothing currently except revert to default
+        if (shape == "wave" || shape == "double wave")
+        {
+            shape = "static";
+        }
+
+        // other shapes
+        for (int loopNum = 0; loopNum < amountToPlace; loopNum++)
+        {
+            // stop the loop if the list is now empty
+            if (allowableTiles.Count == 0)
+            {
+                break;
+            }
+
+            // get a random index, then delete that possibility from the list
+            int index = Random.Range(0, allowableTiles.Count);
+            Vector2Int newTile = allowableTiles[index];
+
+            affectedTiles.Add(newTile);
+            allowableTiles.RemoveAt(index);
+
+            // the x and y of the initial starting tile for the shape
+            // in the case of static, we skip everything
+            int x = newTile.x;
+            int y = newTile.y;
+
+            // now we're drawing shapes!
+            switch (shape)
+            {
+                case "static":
+                    return affectedTiles;
+                case "plus":
+                    // plus shape
+                    // up, right, down, left
+                    possibleAffectedTiles.Add(new Vector2Int(x, y - 1));
+                    possibleAffectedTiles.Add(new Vector2Int(x + 1, y));
+                    possibleAffectedTiles.Add(new Vector2Int(x, y + 1));
+                    possibleAffectedTiles.Add(new Vector2Int(x - 1, y));
+                    break;
+                case "circle":
+                    // circle, duh
+                    // drawing a square, then a bit extra
+                    // up/down - left, middle, right + same one row above
+                    // right/left - side, then side 3 in same x value
+                    // up
+                    possibleAffectedTiles.Add(new Vector2Int(x - 1, y + 1));
+                    possibleAffectedTiles.Add(new Vector2Int(x, y + 1));
+                    possibleAffectedTiles.Add(new Vector2Int(x + 1, y + 1));
+                    possibleAffectedTiles.Add(new Vector2Int(x - 1, y + 2));
+                    possibleAffectedTiles.Add(new Vector2Int(x, y + 2));
+                    possibleAffectedTiles.Add(new Vector2Int(x - 1, y + 2));
+                    // right
+                    possibleAffectedTiles.Add(new Vector2Int(x + 1, y));
+                    possibleAffectedTiles.Add(new Vector2Int(x + 2, y + 1));
+                    possibleAffectedTiles.Add(new Vector2Int(x + 2, y));
+                    possibleAffectedTiles.Add(new Vector2Int(x + 2, y - 1));
+                    // down
+                    possibleAffectedTiles.Add(new Vector2Int(x - 1, y - 1));
+                    possibleAffectedTiles.Add(new Vector2Int(x, y + 1));
+                    possibleAffectedTiles.Add(new Vector2Int(x + 1, y - 1));
+                    possibleAffectedTiles.Add(new Vector2Int(x - 1, y - 2));
+                    possibleAffectedTiles.Add(new Vector2Int(x, y - 2));
+                    possibleAffectedTiles.Add(new Vector2Int(x - 1, y - 2));
+                    // left
+                    possibleAffectedTiles.Add(new Vector2Int(x - 1, y));
+                    possibleAffectedTiles.Add(new Vector2Int(x - 2, y + 1));
+                    possibleAffectedTiles.Add(new Vector2Int(x - 2, y));
+                    possibleAffectedTiles.Add(new Vector2Int(x - 2, y - 1));
+                    break;
+                case "square":
+                    // wow
+                    // left, middle, and right placement for up and down
+                    // up
+                    possibleAffectedTiles.Add(new Vector2Int(x - 1, y + 1));
+                    possibleAffectedTiles.Add(new Vector2Int(x, y + 1));
+                    possibleAffectedTiles.Add(new Vector2Int(x + 1, y + 1));
+                    // right
+                    possibleAffectedTiles.Add(new Vector2Int(x + 1, y));
+                    // down
+                    possibleAffectedTiles.Add(new Vector2Int(x - 1, y - 1));
+                    possibleAffectedTiles.Add(new Vector2Int(x, y - 1));
+                    possibleAffectedTiles.Add(new Vector2Int(x + 1, y - 1));
+                    // left
+                    possibleAffectedTiles.Add(new Vector2Int(x - 1, y));
+                    break;
+                default:
+                    return affectedTiles;
+            }
+            if (possibleAffectedTiles.Count == 0)
+            {
+                continue;
+            }
+            foreach (Vector2Int location in possibleAffectedTiles)
+            {
+                if (allowableTiles.Contains(location))
+                {
+                    allowableTiles.Remove(location);
+                    affectedTiles.Add(location);
+                }
+                // if this tile isn't able to be affected, nothing happens
+            }
+            // delete all of the stuff we tried placing to avoid duplication on next loop
+            possibleAffectedTiles.Clear();
+        }
+
+        return affectedTiles;
     }
 
     /// <summary>
@@ -147,7 +311,8 @@ public class TileAttack : Attack
             {
                 for (int dy = -1; dy <= 1; dy++)
                 {
-                    if (dx == 0 && dy == 0) continue;
+                    if (dx == 0 && dy == 0)
+                        continue;
 
                     Vector2Int neighbor = new Vector2Int(tile.x + dx, tile.y + dy);
 
