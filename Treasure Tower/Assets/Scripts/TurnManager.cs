878617bbd791f94f3b0aeb6004f93afc0e3889cb
Jayce <jayce.baxter@proton.me>
2026-04-13 12:41:41 -0400

    added turn manager class

---
+++ Treasure Tower/Assets/Scripts/TurnManager.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class TurnManager
+{
+    public enum TurnState
+    {
+        PlayerTurn,
+        BossTurn
+    }
+
+    public TurnState CurrentTurn { get; private set; }
+    private int m_TurnCount;
+
+    public TurnManager()
+    {
+        m_TurnCount = 1;
+        CurrentTurn = TurnState.PlayerTurn;
+    }
+
+    public void Tick()
+    {
+        m_TurnCount += 1;
+
+        if (CurrentTurn == TurnState.PlayerTurn)
+        {
+            CurrentTurn = TurnState.BossTurn;
+        }
+        else
+        {
+            CurrentTurn = TurnState.PlayerTurn;
+        }
+
+        UnityEngine.Debug.Log($"Turn {m_TurnCount} — {CurrentTurn}");
+    }
+
+    public bool IsPlayerTurn()
+    {
+        return CurrentTurn == TurnState.PlayerTurn;
+    }
+}
